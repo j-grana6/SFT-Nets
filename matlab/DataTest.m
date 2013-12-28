412f2db375b99f2740ae03586ae6c949ecd96c3b
Justin Grana <jg3705a@student.american.edu>
2013-12-28 14:58:54 -0500

    Fixed bug in DataTest output

--- matlab/DataTest.m
+++ matlab/DataTest.m
@@ -1,6 +1,6 @@
 %% Define Network.
 T = 10000;  % Total time interval for data sequence
-lambda1=1;  % Rate of non-infected message passage. 
+lambda1=1;  % Rate of non-infected message passage.
 k=0.1;  % Rate of infected message passage.
 nodes=4; % Number of nodes.
 Network = zeros(nodes); %Matrix of rates between the various nodes.
@@ -16,14 +16,14 @@ Data = Generate_Data(T,lambda1,Network);  % Call function to generate simulated 
 % Data.time_n = times at which each node became infected.
 % Data.num = number of messages sent along each link before t = [1,2,...,T]
 % Data.t = number of time points.
-% Data.links(i).vec = actual times of messages sent along the ith link. 
+% Data.links(i).vec = actual times of messages sent along the ith link.
 
 %% Specify Time.
-time_array = [0,10000*rand(1,3)];  %Initial guess for the times at which each node became infected.
-% time_array = Data.time_n; %% Give the correct value of the infections times as the initial guess.
+% time_array = [0,10000*rand(1,3)]  %Initial guess for the times at which each node became infected.
+time_array = Data.time_n %% Give the correct value of the infections times as the initial guess.
 
 %% Compute Probability of that Timing
-Prob_Model_Given_Data = Get_Prob_Model_Given_Data(time_array,Network,Data,lambda1,T);  
+Prob_Model_Given_Data = Get_Prob_Model_Given_Data(time_array,Network,Data,lambda1,T)
 % Call function to compute the probability of the infections times in
 % 'time_array' given the network model and the simulated data sequence.
 
@@ -34,7 +34,7 @@ struct2csv(Data_Except_Links,'test_data/Data_Except_Links.csv');
 m = length(Data.links);
 for i = 1:m
     name = sprintf('test_data/vector%i.csv',i);
-    struct2csv(Data.links,name);
+struct2csv(Data.links(i),name);
 end
 
 fileID = fopen('test_data/Prob_Model_Given_Data.csv','w');
