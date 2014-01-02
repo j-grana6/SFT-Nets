ec61c0273d283c3bd79a92309e7a799257a5eb1d
Dongping24 <xdp668@gmail.com>
2014-01-02 17:23:29 +0800

    dx: a small bug fix in make_plots.py

--- python/make_plots.py
+++ python/make_plots.py
@@ -5,7 +5,7 @@ This script makes plots for simulated results.
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Plot infection times against likelihood difference
+# Plot likelihood difference against infection times
 with open("out/output_att.csv","r") as fh:
 	prob_no_att, prob_with_att, times_A, times_B, times_C, times_D = \
 		np.loadtxt(fh,delimiter=',', unpack=True)
@@ -15,8 +15,8 @@ ax.plot(times_A,prob_diff,"yo",linestyle = " ",label = "A infection times")
 ax.plot(times_B,prob_diff,"rs",linestyle = " ",label = "B infection times")
 ax.plot(times_C,prob_diff,"b*",linestyle = " ",label = "C infection times")
 ax.plot(times_D,prob_diff,"g^",linestyle = " ",label = "D infection times")
-ax.set_xlabel("likelihood difference")
-ax.set_ylabel("infection times")
+ax.set_xlabel("infection times")
+ax.set_ylabel("likelihood difference")
 ax.set_title("Attacker Existence")
 ax.legend(bbox_to_anchor = (1.0,1.0))
 ax.set_yticks(np.arange(-100,175,25))
