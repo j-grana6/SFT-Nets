399e9815130d5230a02b31e582dd5ce16d0875d1
j-grana6 <jg3705a@student.american.edu>
2014-03-06 06:47:36 -0500

    Added ROC script

--- python/testing_net.py
+++ python/testing_net.py
@@ -2,20 +2,21 @@ from sft import *
 from sft_net import *
 
 A = SFT('A', ['normal', 'infected'], ['B', 'C'],
-      {'B':np.array([[1, 0], [1, 1/10000.]]),
-       'C': np.array([[1, 0], [1, 1/10000.]])},
+      {'B':np.array([[1, 0], [.5, 1/10000.]]),
+       'C': np.array([[1, 0], [.5, 1/10000.]])},
        ['clean', 'malicious'], 'external')
 
 B = SFT('B', ['normal', 'infected'], ['C', 'D'],
-       {'C': np.array([[1, 0], [1, .1]]),
+       {'C': np.array([[1, 0], [.5, .01]]),
         'D' : np.array([[1, 0], [1, .1]])},
        ['clean', 'malicious'], 'internal')
 
 C = SFT('C', ['normal', 'infected'], ['D'],
-       {'D': np.array([[1, 0], [1,.1]])},
+       {'D': np.array([[1, 0], [1.5, 2]])},
        ['clean', 'malicious'], 'internal')
 
 D = SFT('D', ['normal', 'infected'], [], {}, [], 'internal')
+
 
 T=10000
 nodes = [A, B, C, D]
